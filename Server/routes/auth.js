2b10112df2e338980d7371defa6d390738e266f1
GovardhanKurli <kurligovardhan@gmail.com>
2025-03-20 15:36:30 +0530

    profile section updated

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,17 +1,10 @@
-//Imports the register and login functions from the authController module. These functions will handle the logic for user registration and login.
 
 const { register, login } = require("../controllers/registerController")
 
 //Creates an Express router instance using express.Router(). This router will be used to define authentication-related routes.
 
 const authRouter = require("express").Router()
-//   register route
-
 authRouter.post('/register', register)
-
-
-//   login route
 authRouter.post('/login', login)
 
-//Exports the authRouter instance so it can be used in other parts of the application, typically in the main server file where all routes are assembled.
 module.exports = authRouter
